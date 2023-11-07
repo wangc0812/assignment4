53f254d5f4f83e705b85bbc4bbc343daff4fcaf4
wangc0812 <leoncongwang@gmail.com>
2023-11-07 10:08:38 +0800

    give up timer

--- LeNet.py
+++ LeNet.py
@@ -27,23 +27,23 @@ class LeNet(torch.nn.Module):
         
         
     def forward(self, x):  
-        eval_start_time = time.time()
+        # eval_start_time = time.time()
         x = self.quant(x) 
-        eval_end_time = time.time()
-        eval_duration_time1 = eval_end_time - eval_start_time
+        # eval_end_time = time.time()
+        # eval_duration_time1 = eval_end_time - eval_start_time
         # print(result)
         
-        eval_start_time = time.time()
+        # eval_start_time = time.time()
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
-        eval_end_time = time.time()
-        eval_duration_time = eval_end_time - eval_start_time
-        print("Evaluate conv computing time (seconds): {0:.6f}".format(eval_duration_time))
+        # eval_end_time = time.time()
+        # eval_duration_time = eval_end_time - eval_start_time
+        # print("Evaluate conv computing time (seconds): {0:.6f}".format(eval_duration_time))
         
-        eval_start_time = time.time() 
+        # eval_start_time = time.time() 
         x = self.dequant(x)
-        eval_end_time = time.time()
-        eval_duration_time2 = eval_end_time - eval_start_time
-        print("Evaluate quant and dequant computing time (seconds): {0:.6f}".format(eval_duration_time1+eval_duration_time2)) 
+        # eval_end_time = time.time()
+        # eval_duration_time2 = eval_end_time - eval_start_time
+        # print("Evaluate quant and dequant computing time (seconds): {0:.6f}".format(eval_duration_time1+eval_duration_time2)) 
         return x
